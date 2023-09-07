5efc502877f0a3e329389f8c5b757c3bee078a50
AshtonUys15 <ashton.dejongh1525@gmail.com>
2023-09-07 12:58:20 +0200

    added js api for forecast

--- index.js
+++ index.js
@@ -86,10 +86,11 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 
-function search(city) {
+function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
